34bd1957faff43b64867ef8bfbf9b0a32a06293a
Akash Maurya <108115438+Itsdevakash@users.noreply.github.com>
2024-09-25 00:28:46 +0530

    Update ThankYou.jsx

--- src/ThankYou.jsx
+++ src/ThankYou.jsx
@@ -2,9 +2,11 @@ import React from "react";
 import { useLocation,Link } from "react-router-dom";
 
 function ThankYou() {
-  const location = useLocation(); 
-
-  const { razorpay_payment_id, razorpay_order_id, amount } = location.state || {};
+  // Get query parameters from URL
+  const searchParams = new URLSearchParams(window.location.search);
+  const razorpay_payment_id = searchParams.get("payment_id");
+  const razorpay_order_id = searchParams.get("order_id");
+  const amount = searchParams.get("amount");
 
   return (
     <div className="thank-you-container">
@@ -21,8 +23,8 @@ function ThankYou() {
           </div>
         )}
         {!razorpay_payment_id && <p className="error-text">Error: No payment details available.</p>}
-
         <Link  to={'/'} className="back-home-button">  Back to Home</Link>
+        
       </div>
     </div>
   );
